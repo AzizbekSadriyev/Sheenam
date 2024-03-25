b6af386086107450b0e888be19a54d33d294c2ef
Azizbek Sadriyev <azizbeksadriyev@gmail.com>
2024-03-25 22:08:10 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
